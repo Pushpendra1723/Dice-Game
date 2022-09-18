b8c9db3f4b52dc91f4a92647f61e5ef055b1b7b4
Pushpendra_Kumar <94159743+Pushpendra1723@users.noreply.github.com>
2022-09-18 22:53:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,8 +75,8 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    // 2. Check if player's score is >= 50
+    if (scores[activePlayer] >= 50) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
